a00af6a4cab94a6684e9a1f3aeb177c3486fc69b
61130061 <thezeroyou@gmail.com>
2022-10-26 14:40:23 +0700

    🔨 fix last commit issue

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Command from './components/Command';
 import Boot from './components/Boot';
 
 function App() {
-   const [isBooting, setBooting] = useState(false);
+   const [isBooting, setBooting] = useState(true);
 
    return (
       <div className="text-white overflow-x-hidden md:text-[16px] text-sm leading-[18px] md:p-3 p-1">

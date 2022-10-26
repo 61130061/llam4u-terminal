fd5362865e0a56ce561992826384e98d3aa09728
61130061 <thezeroyou@gmail.com>
2022-10-26 11:04:53 +0700

    ♻️ update whoami art & fix title & remove initial test file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import Command from './components/Command';
 import Boot from './components/Boot';
@@ -6,12 +6,10 @@ import Boot from './components/Boot';
 function App() {
    const [isBooting, setBooting] = useState(true);
 
-   document.title = 'Llam4u Terminal'
-
    return (
       <div className="text-white overflow-x-hidden md:text-[16px] text-sm leading-[18px] md:p-3 p-1">
          {isBooting ?
-            <Boot setBoot={setBooting}  />:
+            <Boot setBoot={setBooting} /> :
             <Command />
          }
       </div>
